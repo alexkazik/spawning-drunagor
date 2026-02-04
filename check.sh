743ffb79f89cf2c3e6effa22a51c3ee325d31e62
Alex Kazik <alex@kazik.de>
2026-02-04 10:23:26 +0100

    Improve check script

--- check.sh
+++ check.sh
@@ -3,8 +3,9 @@ set -e # exit on error
 set -x # show commands executed
 cargo build --target wasm32-unknown-unknown --release --no-default-features
 cargo build --target wasm32-unknown-unknown --release
+cargo clippy --target wasm32-unknown-unknown --release --no-default-features
 cargo clippy --target wasm32-unknown-unknown --release
-cargo +nightly clippy --target wasm32-unknown-unknown --release
+cargo build --bin copy-and-link
 for width in 500 400 300 200 150 130 110
 do
   cargo +nightly fmt --all -- --config max_width=$width
